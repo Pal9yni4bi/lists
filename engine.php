89c3f845130d07c23d8199ce9df6f88cf2e7b5b0
Pal9yni4bi <pal9yni4bi@gmail.com>
2015-10-21 15:50:47 +0300

    a little cleaning of code

--- engine.php
+++ engine.php
@@ -1,14 +1,7 @@
 <?
-
-// echo json_encode($arr);
- // $other_mass = json_encode($_POST); //генерация значения
- // $other_mass = json_encode($_POST["checks"]); //генерация значения
- // $other_mass = json_encode($_POST["lists"]); //генерация значения
  $lists = $_POST["lists"]; //генерация значения
      
      $f = fopen('lists.json', 'w');    //открываем файл
-     fwrite($f, $lists);       // записываем в него сгенерированное значение
+     fwrite($f, $lists_clear);       // записываем в него сгенерированное значение
      fclose($f);
-	 
-	 echo $lists;
 ?>
