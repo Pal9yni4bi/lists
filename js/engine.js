dc9a0a9a6cdbad56aab26c76ce10904283a41401
Pal9yni4bi <pal9yni4bi@gmail.com>
2015-10-22 10:58:42 +0300

    bages (number of items in every list) works correctly; bind() changed to on(); some bugs fixed

--- js/engine.js
+++ js/engine.js
@@ -29,15 +29,16 @@ function list_adding(name) {
 	var $id = $(".lists-names-group li").length - 1;
 	$(".lists-names-group li").eq(-1).before("<li role=\"presentation\"><a href=\"#list"+ $id +"\" aria-controls=\"messages\" role=\"tab\" data-toggle=\"tab\"><span class=\"lists-name\">"+ name +"</span> <span class=\"badge\"></span></a></li>");
 	$(".tab-pane").eq(-1).before("<div role=\"tabpanel\" class=\"tab-pane\" id=\"list"+$id+"\"><ul class=\"list-group\"></ul><div class=\"row\"><div class=\"col-lg-12\"><div class=\"input-group\"><input type=\"text\" class=\"form-control\" placeholder=\"List item adding\"><span class=\"input-group-btn\"><button class=\"btn btn-default add-button\" type=\"button\">Add</button></span></div><!-- /input-group --></div><!-- /.col-lg-12 --></div><!-- /.row --></div>");
-	$(".tab-pane").eq(-2).find(".add-button").bind("click", item_adding);
+	$(".tab-pane").eq(-2).find(".add-button").on("click", item_adding);
 	count_items_in_list($id);
 	save();
 }
 function item_adding() {
 	var $current_tab = $(this).parent().parent().parent().parent().parent();
 	var $input = $current_tab.find("input").val();
-	$current_tab.children("ul.list-group").append("<li class=\"list-group-item\"><div class=\"row\"><div class=\"col-xs-10\">" + $input +"</div><div class=\"col-xs-2 buttons-area\"></div></div></li>");
+	$current_tab.children("ul.list-group").append("<li class=\"list-group-item\"><div class=\"row\"><div class=\"col-xs-10 item-text\">" + $input +"</div><div class=\"col-xs-2 buttons-area\"></div></div></li>");
 	make_change_button_alive();
+	count_items_in_list($current_tab.index());
 	save();
 }
 function edit_text() {
@@ -69,10 +70,10 @@ function delete_item() {
 }
 function make_change_button_alive() {
 	$(".buttons-area").html("<div class=\"btn-group pull-right\"><button type=\"button\" class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">Change <span class=\"caret\"></span></button><ul class=\"dropdown-menu\"><li class=\"edit-text\"><a href=\"#\">Edit text</a></li><li class=\"mark-as-done\"><a href=\"#\">Mark as done</a></li><li class=\"mark-as-undone\"><a href=\"#\">Mark as undone</a></li><li role=\"separator\" class=\"divider\"></li><li class=\"delete\"><a href=\"#\">Delete</a></li></ul></div>");
-	$(".edit-text").bind("click", edit_text);
-	$(".mark-as-done").bind("click", mark_as_done);
-	$(".mark-as-undone").bind("click", mark_as_undone);
-	$(".delete").bind("click", delete_item);
+	$(".edit-text").on("click", edit_text);
+	$(".mark-as-done").on("click", mark_as_done);
+	$(".mark-as-undone").on("click", mark_as_undone);
+	$(".delete").on("click", delete_item);
 }
 function count_items_in_list(i) {
 	var number_of_items = $(".list-group").eq(i).find(".item-text").length;
