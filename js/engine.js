948c06874a67cdfc5fb699d242c086c168b4b6f8
Pal9yni4bi <pal9yni4bi@gmail.com>
2015-10-24 01:09:01 +0300

    fix errors with saving mistake in boxex under editing until save function activated by another place

--- js/engine.js
+++ js/engine.js
@@ -25,17 +25,17 @@ $.getJSON('../data.json', function(data){
 	console.log( [ xhr.status, textStatus ] );
 	$error_box.html("Data hadn't loaded successfull").slideToggle(500).fadeOut(3000);
 });
-$(".add-list-button").click(function(name) {
+$(".add-list-button").on("click", function(name) {
 	list_adding($(this).parent().parent().children().val());
 	save();
 });
-$(".add-button").click(item_adding);
-$(".edit-list-name").click(list_name_edit);
-$(".delete-list").click(list_delete);
-$(".edit-text").click(edit_text);
-$(".mark-as-done").click(mark_as_done);
-$(".mark-as-undone").click(mark_as_undone);
-$(".delete").click(delete_item);
+$(".add-button").on("click", item_adding);
+$(".edit-list-name").on("click", list_name_edit);
+$(".delete-list").on("click", list_delete);
+$(".edit-text").on("click", edit_text);
+$(".mark-as-done").on("click", mark_as_done);
+$(".mark-as-undone").on("click", mark_as_undone);
+$(".delete").on("click", delete_item);
 function list_adding(name) {
 	var $id = $(".lists-names-group li").length - 1;
 	if ($id == 0) {
@@ -45,6 +45,7 @@ function list_adding(name) {
 		$(".lists-names-group li").eq(-1).before("<li role=\"presentation\"><a href=\"#list"+ $id +"\" aria-controls=\"messages\" role=\"tab\" data-toggle=\"tab\"><span class=\"lists-name\">"+ name +"</span> <span class=\"badge\"></span></a></li>");
 		$(".tab-pane").eq(-1).before("<div role=\"tabpanel\" class=\"tab-pane\" id=\"list"+$id+"\"><ul class=\"list-group\"></ul><div class=\"row\"><div class=\"col-lg-12\"><div class=\"input-group\"><input type=\"text\" class=\"form-control\" placeholder=\"List item adding\"><span class=\"input-group-btn\"><button class=\"btn btn-default add-button\" type=\"button\">Add</button></span></div></div></div></div>");
 	}
+	
 	$(".tab-pane").eq(-2).find(".add-button").on("click", item_adding);
 	count_items_in_list($id);
 }
@@ -53,8 +54,7 @@ function list_name_edit(){
 	console.log("list name editing");
 	var $current_tab = $(".lists-names-group .active .lists-name");
 	var $current_name = $current_tab.text();
-	$current_tab.html("<div class=\"input-group\"><input type =\"text\" class=\"form-control\" value=\"" + $current_name + "\"><span class=\"input-group-btn\"><button class=\"btn btn-default pull-right\" type=\"submit\">Submit</button></span></div>");
-	$current_tab.find("button").on("click", function() {
+	$current_tab.html("<div class=\"input-group\"><input type =\"text\" class=\"form-control\" value=\"" + $current_name + "\"><span class=\"input-group-btn\"><button class=\"btn btn-default pull-right submit-button\" type=\"submit\">Submit</button></span></div>").find("button").on("click", function() {
 		$current_tab.text($current_tab.find("input").val());
 		$(".edit-list-name").removeClass("disabled").on("click", list_name_edit);
 		save();
@@ -74,8 +74,7 @@ function list_delete() {
 function item_adding() {
 	var $current_tab = $(this).parent().parent().parent().parent().parent();
 	var $input = $current_tab.find("input").val();
-	$added_item = $current_tab.children("ul.list-group").append("<li class=\"list-group-item\"><div class=\"row\"><div class=\"col-xs-10 item-text\">" + $input +"</div><div class=\"col-xs-2 buttons-area\"></div></div></li>");
-	$added_item.find(".buttons-area").html($item_edit_button);
+	$current_tab.children("ul.list-group").append("<li class=\"list-group-item\"><div class=\"row\"><div class=\"col-xs-10 item-text\">" + $input +"</div><div class=\"col-xs-2 buttons-area\"></div></div></li>").find(".buttons-area").html($item_edit_button);
 	make_change_button_alive();
 	count_items_in_list($current_tab.index());
 	save();
@@ -84,13 +83,13 @@ function edit_text() {
 	var $current_item = $(this).parent().parent().parent().parent().children(".item-text");
 	var $edited_text = $current_item.text();
 	$current_item.html("<input type=\"text\" class=\"form-control\" value=\""+$edited_text+"\">");
-	$current_item.parent().children(".buttons-area").html("<button class=\"btn btn-default pull-right\" type=\"submit\">Submit</button>");
+	$current_item.parent().children(".buttons-area").html("<button class=\"btn btn-default pull-right submit-button\" type=\"submit\">Submit</button>");
 	$current_item.parent().children(".buttons-area").children().on("click", function() {
 		$new_text = $(this).parent().parent().children(".item-text").children("input").val();
 		$(this).parent().parent().children(".item-text").text($new_text);
 		$(this).parent().parent().children(".buttons-area").html($item_edit_button);
 		make_change_button_alive();//add context for exclude doubling?
-		save();		
+		save();
 	});
 }
 function mark_as_done() {
@@ -120,21 +119,22 @@ function count_items_in_list(i) {
 	$(".lists-name").eq(i).parent().find(".badge").html(number_of_items);
 }
 function save() {
-var lists = {};
-$(".lists-names-group .lists-name").each(function(indx,elem) {
-	lists[$(elem).text()] = [];
-	var $current_list = lists[$(elem).text()];
-	$(".tab-pane").eq(indx).find(".item-text").each(function(i, e) {
-		lists[$(elem).text()][i] = [];
-		lists[$(elem).text()][i][0] = $(e).html(); //write text of list item to memory
-		lists[$(elem).text()][i][1] = "0"; //write 0-parameter (=undone) by default
-		if ($(e).parent().parent().hasClass("list-group-item-success")) {
-			lists[$(elem).text()][i][1] = "1"; //write 1-parameter (=done), if item marked as done
-		}
+	$(".submit-button").click();
+	var lists = {};
+	$(".lists-names-group .lists-name").each(function(indx,elem) {
+		lists[$(elem).text()] = [];
+		var $current_list = lists[$(elem).text()];
+		$(".tab-pane").eq(indx).find(".item-text").each(function(i, e) {
+			lists[$(elem).text()][i] = [];
+			lists[$(elem).text()][i][0] = $(e).html(); //write text of list item to memory
+			lists[$(elem).text()][i][1] = "0"; //write 0-parameter (=undone) by default
+			if ($(e).parent().parent().hasClass("list-group-item-success")) {
+				lists[$(elem).text()][i][1] = "1"; //write 1-parameter (=done), if item marked as done
+			}
+		});
 	});
-});
-var data=JSON.stringify(lists);
-$.ajax({
+	var data=JSON.stringify(lists);
+	$.ajax({
 		type: 'POST',
 		url: "engine.php",
 		cache: false,
